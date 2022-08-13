ef152046160832cfd5ce6c758af0055411b8243a
CreeperJIMM <67496191+CreeperJIMM@users.noreply.github.com>
2022-08-13 22:01:16 +0800

    test

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+var Lang = document.cookie.replace(/(?:(?:^|.*;\s*)i18nLang\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+if(!Lang && navigator.language) {
+    document.cookie += `i18nLang=${navigator.language}`
+}
