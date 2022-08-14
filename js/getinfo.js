7ef93dede9e868516a1f9ec7503f5de000732205
CreeperJIMM <67496191+CreeperJIMM@users.noreply.github.com>
2022-08-14 10:32:40 +0800

    test

--- js/getinfo.js
+++ js/getinfo.js
@@ -1,13 +1,26 @@
-let errorObj = {ok:false,data: null}
-var getData = async(option) => {
- return {Error:"hi"}
-}
-var iflogin = async() =>  {
+let errorObj = { ok: false, data: null }
+let baseURL = "https://api.chinohelper.xyz"
+
+var iflogin = async () => {
     try {
-        let data =  await (await fetch('/api/user',{method: "POST",body: new URLSearchParams({
-            "uid": id
-        })})).json()
+        let data = await (await fetch(`${baseURL}/user/dc/haslogin`, { method: "GET" })).json()
         return data
+    } catch (error) {
+        console.log(error)
+        return errorObj;
+    }
+}
+var getData = async (option) => {
+    try {
+        let body = null
+        if(option) {
+            option = JSON.stringify(option)
+            body = new URLSearchParams({option})
+        }
+        let data = await (await fetch(`${baseURL}/user/dc/data`, {
+            method: "POST", body: body
+        })).json()
+        return data;
     } catch (error) {
         console.log(error)
         return errorObj;
