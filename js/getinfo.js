d3aa7b684c99eec5ae32a41bff34fffcc74e9a6c
CreeperJIMM <67496191+CreeperJIMM@users.noreply.github.com>
2022-08-16 23:06:53 +0800

    login page

--- js/getinfo.js
+++ js/getinfo.js
@@ -68,3 +68,29 @@ var getEmail = async () => {
         return errorObj;
     }
 }
+var getUserVlaue = async () => {
+    try {
+        let data = await (await fetch(`${baseURL}/user/db/value`, {
+            method: "GET",
+            credentials: 'include',
+            headers: headers
+        })).json()
+        return data
+    } catch (error) {
+        console.log(error)
+        return errorObj;
+    }
+}
+var getUserData = async () => {
+    try {
+        let data = await (await fetch(`${baseURL}/user/db/data`, {
+            method: "GET",
+            credentials: 'include',
+            headers: headers
+        })).json()
+        return data
+    } catch (error) {
+        console.log(error)
+        return errorObj;
+    }
+}
